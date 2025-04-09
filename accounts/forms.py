cc0e2ab7dca946022e7c9122a00a0a41145bd87b
Alexander-Bakshaev <shuriknt@gmail.com>
2025-04-09 14:50:35 +0500

    Пользовательские поля модели User

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+from .models import Profile
+
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100)
@@ -24,3 +26,24 @@ class LoginForm(AuthenticationForm):
                                required=True,
                                widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
     remember_me = forms.BooleanField(required=False)
+
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput())
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput())
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput())
+    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
-from .forms import SignUpForm, LoginForm
+from .forms import SignUpForm, LoginForm, UpdateUserForm, UpdateProfileForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib import messages
@@ -32,7 +34,7 @@ class SignUpView(generic.CreateView):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
 
-            return redirect(to='login') # редирект на страницу логина после регистрации
+            return redirect(to='login')  # редирект на страницу логина после регистрации
 
         return render(request, self.template_name, {'form': form})
 
@@ -50,3 +52,27 @@ class CustomLoginView(LoginView):
 
         # В противном случае сеанс браузера будет таким же как время сеанса cookie "SESSION_COOKIE_AGE", определенное в settings.py
         return super(CustomLoginView, self).form_valid(form)
+
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'registration/change_password.html'
+    success_message = "Successfully Changed Your Password"
+    success_url = reverse_lazy('users-profile')
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'registration/profile.html', {'user_form': user_form, 'profile_form': profile_form})
