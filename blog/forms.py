e60ad90ea2c84742db810e84a5087f00b8e845b8
Alexander-Bakshaev <shuriknt@gmail.com>
2025-04-10 12:43:09 +0500

    Улучшен дизайн блога с использованием Bootstrap 5

--- blog/forms.py
+++ blog/forms.py
@@ -3,22 +3,25 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    name = forms.CharField(max_length=25, required=True, widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Name'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'E-Mail'}))
+    to = forms.EmailField(required=True, widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'To'}))
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea(attrs={"class": "form-control mb-1", 'placeholder': 'Comments'}))
 
 
 class CommentForm(forms.ModelForm):
+    name = forms.CharField(required=True,
+                           widget=forms.TextInput(attrs={"class": "form-control", 'placeholder': 'Имя'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.EmailInput(attrs={"class": "form-control", 'placeholder': 'Email'}))
+    body = forms.CharField(required=True, widget=forms.Textarea(attrs={"class": "form-control", 'placeholder': 'Текст'}))
+
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
-        widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Username'}),
-            'email': forms.PasswordInput(attrs={'placeholder': 'Email'}),
-            'body': forms.Textarea(attrs={'placeholder': 'Текст комментария'}),
-        }
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label='Поиск')
+    query = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Enter search term...'}))
